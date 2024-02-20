ace9952ea3bf5a8b1e9997f385f60ca1ac8f47d8
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2024-02-20 03:49:08 -0500

    add redux-thunk as redux store middleware

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { createStore } from "redux";
+import {
+  createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
+import { thunk } from "redux-thunk";
+
+const composeEnhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+  compose;
 
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancer(
+      applyMiddleware(thunk)
+    )
 );
 ReactDOM.createRoot(
   document.getElementById("root")
